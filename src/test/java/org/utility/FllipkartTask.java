3ace7211565a76521f230b2f1b52de433036b5dc
kaviragava63 <kaviyarasanragava64@gmail.com>
2022-11-08 20:15:35 -0800

    project structure

--- src/test/java/org/utility/FllipkartTask.java
+++ src/test/java/org/utility/FllipkartTask.java
@@ -1,5 +1,10 @@
 package org.utility;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.server.handler.SendKeys;
@@ -10,7 +15,7 @@ public class FllipkartTask extends BaseClass  {
 	
 	@Test
 	
-	private void browserLaunch() {
+	private void flipkartProducts() {
 		
 		browserLaunch("chrome");
 		urlLaunch("https://www.flipkart.com/");
@@ -24,15 +29,66 @@ public class FllipkartTask extends BaseClass  {
 		
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		
+		////div[@class='_4rR01T'] ₹40,990
+		
+		
+		List<WebElement> product = driver.findElements(By.xpath("//div[@class='_4rR01T']"));
+		WebElement n;
+		for (int i=0;i<product.size();i++) {
+			n = product.get(i);
+			System.out.println(n.getText());
+		}
+		
+		System.out.println("....price....");
+		
+		List<WebElement> price = driver.findElements(By.xpath("//div[@class='_30jeq3 _1_WHN1']"));
+		List<Integer> li = new LinkedList<>();
+		String rp="";
+		
+		for (int j=0;j<price.size();j++) {
+		     String tx = price.get(j).getText();
+		     tx.contains("₹");
+		     rp = tx.replace("₹", "");
+		     //tx.contains(",");
+		     //rp = tx.replace(",", "");
+		     
+		   
+		     
+		    
+		     System.out.println(rp);
+			    /*Integer min = Collections.min(li);
+				Integer max = Collections.max(li);
+				System.out.println(min);
+				System.out.println(max);*/
+		    
+			}
+		
+	
 		
 		
 		
 		
 		
+	      /*  Collections.sort(li);
+		    Integer min = Collections.min(li);
+			Integer max = Collections.max(li);
+			System.out.println(min);
+			System.out.println(max);
+			*/
+			
+			
+			
+			
+			
+			
+		}
 		
 		
+
 		 
 		
+	
+	
 		
 		
 		
@@ -41,4 +97,4 @@ public class FllipkartTask extends BaseClass  {
 	
 	
 
-}
+
